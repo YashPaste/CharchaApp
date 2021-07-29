33ae5b2fba315e8977b550cde28e7369526c3f30
YASH PASTE <yashshinde2110@gmail.com>
2021-07-29 17:43:15 +0530

    Modified login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
+import { GoogleOutlined, AliwangwangOutlined} from '@ant-design/icons'
 
 import firebase from "firebase/app"
 
@@ -10,22 +10,13 @@ export default function Login() {
   return (
     <div id='login-page'>
       <div id='login-card'>
-        <h2>Welcome to Unichat!</h2>
+        <h2>Welcome to Charcha! <AliwangwangOutlined /></h2>
 
         <div
           className='login-button google'
           onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
         >
           <GoogleOutlined /> Sign In with Google
-        </div>
-
-        <br/><br/>
-
-        <div
-          className='login-button facebook'
-          onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider()) }
-        >
-          <FacebookOutlined /> Sign In with Facebook
         </div>
       </div>
     </div>
